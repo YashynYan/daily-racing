ac5d9327b88f6bbca5f40281843df56230fca709
Yan Yashyn <yan.yashyn@contractors.medallia.com>
2021-11-29 12:29:11 +0200

    add functionality

--- login/script.js
+++ login/script.js
@@ -13,9 +13,20 @@ function showPassword() {
   const eyeIcon = document.getElementById("eye-icon");
   if (passwordField.type === "password") {
     passwordField.type = "text";
-    eyeIcon.src = "../assets/icons/eye-icon-closed.svg"
+    eyeIcon.src = "../assets/icons/eye-icon-closed.svg";
   } else {
     passwordField.type = "password";
-    eyeIcon.src = "../assets/icons/eye-icon.svg"
+    eyeIcon.src = "../assets/icons/eye-icon.svg";
   }
+}
+
+function setError() {
+  const passwordField = document.getElementById("password-field");
+  passwordField.classList.add("invalid-field");
+
+  const usernameField = document.getElementById("username-field");
+  usernameField.classList.add("invalid-field");
+
+  const errorMessage = document.getElementById("error-field");
+  errorMessage.classList.remove('hidden')
 }
